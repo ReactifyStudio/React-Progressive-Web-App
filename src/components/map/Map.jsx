149388543b823730c001bcc3966ce275919b70ea
ReactifyStudio <winchygichu@gmail.com>
2021-03-28 23:28:44 +0300

    added vivid desc

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -5,6 +5,13 @@ import locationIcon from '@iconify/icons-mdi/map-marker'
 
 import './map.css'
 
+// google-map-react || @iconify/react(Icon) @iconify/icons-mdi/map-marker
+// Add location pin ==> Pass a text prop(Will come from the address)
+// The component LocationPin need a text and Icon component will recieve locationIcon as a prop
+// The Map component will take a prop of lication and zoomLevel props
+// Add GoogleMap React with defaults and
+// LocationPin HOC created above
+
 const LocationPin = ({ text }) => (
   <div className="pin">
     <Icon icon={locationIcon} className="pin-icon" />
