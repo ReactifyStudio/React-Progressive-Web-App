75a1d1ff9418fd44a5005ea6ca91ddf932b72e27
ReactifyStudio <winchygichu@gmail.com>
2021-03-28 19:03:22 +0300

    huge code refactor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React,{useState} from 'react';
 import './App.css';
 
-import Form from './components/Form';
-import TodoList from './components/TodoList';
 
 const App=()=>{
 
-  const [todos,setTodos]=useState([]);
-
-  const addTodo= todo=>{
-          const newTodos=[todo,...todos]
-           setTodos(newTodos)
-       }
-  
   return (
     <div>
-      <h1>TODO PWA</h1>
-      <Form addTodo={addTodo}/>
-      <TodoList todos={todos} setTodos={setTodos}/>
+      <h1>REACT GOOGLE MAPS</h1>
+
     </div>
   );
 }
